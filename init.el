c6cfc42b40e5e4152c372567f7a7e8d7d01def9e
Vincent Parizet <vincent.parizet@hotmail.fr>
2019-01-18 12:01:42 +0100

    Bytecompile the config

--- init.el
+++ init.el
@@ -1,8 +1,13 @@
 ;; Don't attempt to find/apply special file handlers to files loaded during startup.
-(let ((file-name-handler-alist nil))
+(let ((file-name-handler-alist nil)
+	  (config-regular (expand-file-name "config.el" user-emacs-directory))
+	  (config-compiled (expand-file-name "config.elc" user-emacs-directory))
+	  (config-org (expand-file-name "config.org" user-emacs-directory)))
+
   ;; If config is pre-compiled, then load that
-  (if (file-exists-p (expand-file-name "config.elc" user-emacs-directory))
-      (load-file (expand-file-name "config.elc" user-emacs-directory))
+  (if (file-exists-p config-compiled)
+      (load-file config-compiled)
     ;; Otherwise use org-babel to tangle and load the configuration
     (require 'org)
-    (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))))
+    (org-babel-load-file config-org)
+    (byte-compile-file config-regular)))
