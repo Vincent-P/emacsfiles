8c1ff546d3524667a1a0f96c1a86ef0d5012bb0d
Vincent Parizet <vincent.parizet@hotmail.fr>
2020-12-15 23:45:26 +0100

    Remove space

--- init.el
+++ init.el
@@ -326,7 +326,7 @@ Arguments the same as in `compile'."
         org-roam-capture-ref-templates '(("r" "ref" plain (function org-roam--capture-get-point)
                                           "%?"
                                           :file-name "%<%Y%m%d%H%M%S>-${slug}"
-                                          :head "#+TITLE: ${title}\n#+CREATED: %U\n#+LAST_MODIFIED: %U\n #+ROAM_KEY: ${ref}\n#+ROAM_ALIAS: \n\n- link :: [[${ref}]]\n- tags :: "
+                                          :head "#+TITLE: ${title}\n#+CREATED: %U\n#+LAST_MODIFIED: %U\n#+ROAM_KEY: ${ref}\n#+ROAM_ALIAS: \n\n- link :: [[${ref}]]\n- tags :: "
                                           :unnarrowed t)))
   )
 
